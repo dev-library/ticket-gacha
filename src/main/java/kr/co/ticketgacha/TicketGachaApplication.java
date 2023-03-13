cf18f81e06e09681dda21cf5628a315f0f4581a1
dev-library <dev.rary@gmail.com>
2023-03-13 22:22:09 +0900

    데브러리 : 프로젝트 세팅

---
+++ src/main/java/kr/co/ticketgacha/TicketGachaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ticketgacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketGachaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketGachaApplication.class, args);
+    }
+
+}
